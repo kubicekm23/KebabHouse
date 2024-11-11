764bc4d3dda8234524d760a01396f564a5964c04
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2024-11-11 13:18:17 +0100

    Přidání odebírání ingrediencí ze skladu
    Nekontroluje zda jich je dost, dokonce jde i do mínusu.

--- KebabHouse/KebabHouse/Classes/Sklad.cs
+++ KebabHouse/KebabHouse/Classes/Sklad.cs
@@ -2,13 +2,43 @@ namespace KebabHouse.Classes;
 
 public class Sklad
 {
+    private int _chleba;
+    private int _tortila;
+    private int _omackaCesnek;
+    private int _omackaBylinka;
+    private int _omackaChilli;
+    private int _maso;
+    private int _mrkev;
+    private int _cibule;
+    private int _zeli;
     public Sklad()
     {
+        _chleba = 0;
+        _tortila = 0;
         
+        _omackaCesnek = 0;
+        _omackaBylinka = 0;
+        _omackaChilli = 0;
+        
+        _mrkev = 0;
+        _cibule = 0;
+        _zeli = 0;
+
+        _maso = 0;
+
     }
 
     public void odebratZeSkladu(Kebab objednavka)
     {
-        
+        Dictionary<string, int> ingredience = objednavka.getIngredience();
+        _chleba -= ingredience["chleba"];
+        _tortila -= ingredience["tortila"];
+        _omackaBylinka -= ingredience["omackaBylinka"];
+        _omackaChilli -= ingredience["omackaChilli"];
+        _omackaCesnek -= ingredience["omackaCesnek"];
+        _maso -= ingredience["maso"];
+        _zeli -= ingredience["zeli"];
+        _cibule -= ingredience["cibule"];
+        _mrkev -= ingredience["mrkev"];
     }
 }
